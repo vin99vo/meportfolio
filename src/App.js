9ec3dad56ed45a8a99435342bd7598ea78741e22
vincent99vo <100818813+vincent99vo@users.noreply.github.com>
2023-03-15 01:29:01 +1100

    change document title

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Body from "./components/body/Body";
 // import Footer from "./components/footer/Footer";
+import React, { useEffect } from "react";
 
 const App = () => {
   // const creator = {
   //   author: "Vincent Vo",
   //   lastUpdate: "March 2023"
   // }
+
+  useEffect(() => {
+    document.title = "Vincent Vo | Portfolio";
+  }, []);
+
   return (
     <div className="App">
-        <Header/>
-        <Body />
-        {/* <Footer/> */}
+      <Header />
+      <Body />
+      {/* <Footer/> */}
     </div>
   );
-}
+};
 
 export default App;
