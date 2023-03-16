e869ee1606512388bbb5dd0ee57ca1a92d07fa14
Kenny Ye <kennyye1998@gmail.com>
2023-03-16 23:11:33 +1100

    test commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Body from "./components/body/Body";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <Router>
+        <route path="/">
+          <Portfolio />
+        </route>
+        <route path="/weather">
+          <WeatherApp />
+        </route>
+        <route path="/project"></route>
+      </Router>
+    </div>
   </React.StrictMode>
 );
-
---
+++ src/portfolio/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Component1 } from "./components/component-1"
+import { Component2 } from "./components/component-2"
+...
+
+export const Portfolio = () => {
+  console.log("i am portfolio");
+
+  let
+  const
+  var 
+
+  
+  const hello = () => {
+
+  }
+
+  function hello {
+
+  }
+
+  array.reduce(() => {})
+  array.map (() => {})
+  array.filter(() => {})
+
+  return (
+    <div>
+      <Component1 />
+      <Component2 />
+      <Component3 />
+      <Component4 />
+      <Component5 />
+    </div>
+  )
+}
