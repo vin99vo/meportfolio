c4abf49244a67d2ed6353b9a38bc5d32445edc6b
vincent99vo <100818813+vincent99vo@users.noreply.github.com>
2023-03-31 16:32:00 +1100

    Updates

--- src/components/general/Description/index.tsx
+++
@@ -1,9 +0,0 @@
-import { type FC } from 'react'
-
-type DescriptionProps = {
-  description: string
-}
-
-export const Description: FC<DescriptionProps> = ({ description }) => {
-  return <p>{description}</p>
-}
